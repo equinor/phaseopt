aee70b1e110dddaec3a33f375dccc284864495fb
Roy Vegard Ovesen <rovo@statoil.com>
2015-06-01 14:47:33 +0200

    Make non-static class

---
+++ Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Main_Class
+{
+    public static void Main(String[] args)
+    {
+        bool Test_UMR = false;
+        foreach (string arg in args)
+        {
+            if (arg.Equals(@"/u"))
+            {
+                Test_UMR = true;
+            }
+        }
+
+        if (Test_UMR)
+        {
+            int[] IDs = new int[22] { 1, 2, 3, 4, 5, 6, 7, 10, 11, 16, 17, 18, 701, 705, 707, 801, 806, 810, 901, 906, 911, 101101 };
+            double[] Values = new double[22] { 2.483, 0.738, 81.667, 8.393, 4.22, 0.605, 1.084, 0.24, 0.23, 0.0801, 0.0243, 0.0614,
+                0.0233, 0.0778, 0.0191, 0.0048, 0.0302, 0.0116, 0.0023, 0.0017, 0.0022, 0.0014};
+
+
+            double[] Result = PhaseOpt.PhaseOpt.Calculate_Dew_Point_Line(IDs, Values, 5);
+
+            Console.WriteLine("Cricondenbar point");
+            Console.WriteLine("Pressure: {0} barg", Result[0].ToString());
+            Console.WriteLine("Temperature: {0} °C", Result[1].ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Cricondentherm point");
+            Console.WriteLine("Pressure: {0} barg", Result[2].ToString());
+            Console.WriteLine("Temperature: {0} °C", Result[3].ToString());
+
+            Console.WriteLine("Dew Point Line");
+            for (int i = 4; i < Result.Length; i += 2)
+            {
+                Console.WriteLine("Pressure: {0} barg", Result[i].ToString());
+                Console.WriteLine("Temperature: {0} °C", Result[i + 1].ToString());
+                Console.WriteLine();
+            }
+
+            double[] Dens_Result = PhaseOpt.PhaseOpt.Calculate_Density_And_Compressibility(IDs, Values);
+
+            Console.WriteLine("Vapour density: {0} kg/m­³", Dens_Result[0]);
+            Console.WriteLine("Compressibility factor: {0}", Dens_Result[2]);
+            Console.WriteLine("Liquid density: {0} kg/m­³", Dens_Result[1]);
+            Console.WriteLine("Compressibility factor: {0}", Dens_Result[3]);
+
+            return;
+        }
+
+        PhaseOpt_KAR GC_A = new PhaseOpt_KAR();
+        PhaseOpt_KAR GC_B = new PhaseOpt_KAR();
+
+        GC_A.Calculate();
+
+    }
+}
+
--- PhaseOpt_KAR.cs
+++ PhaseOpt_KAR.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
 
-public static class Tester
+public class PhaseOpt_KAR
 {
     public class Component
     {
@@ -53,52 +53,8 @@ public static class Tester
     private static string IP21_Uid;
     private static string IP21_Pwd;
 
-    public static void Main(String[] args)
+    public void Calculate()
     {
-        bool Test_UMR = false;
-        foreach (string arg in args)
-        {
-            if (arg.Equals(@"/u"))
-            {
-                Test_UMR = true;
-            }
-        }
-
-        if (Test_UMR)
-        {
-            int[] IDs = new int[22] { 1, 2, 3, 4, 5, 6, 7, 10, 11, 16, 17, 18, 701, 705, 707, 801, 806, 810, 901, 906, 911, 101101 };
-            double[] Values = new double[22] { 2.483, 0.738, 81.667, 8.393, 4.22, 0.605, 1.084, 0.24, 0.23, 0.0801, 0.0243, 0.0614,
-                0.0233, 0.0778, 0.0191, 0.0048, 0.0302, 0.0116, 0.0023, 0.0017, 0.0022, 0.0014};
-
-
-            double[] Result = PhaseOpt.PhaseOpt.Calculate_Dew_Point_Line(IDs, Values, 5);
-
-            Console.WriteLine("Cricondenbar point");
-            Console.WriteLine("Pressure: {0} barg", Result[0].ToString());
-            Console.WriteLine("Temperature: {0} °C", Result[1].ToString());
-            Console.WriteLine();
-
-            Console.WriteLine("Cricondentherm point");
-            Console.WriteLine("Pressure: {0} barg", Result[2].ToString());
-            Console.WriteLine("Temperature: {0} °C", Result[3].ToString());
-
-            Console.WriteLine("Dew Point Line");
-            for (int i = 4; i < Result.Length; i += 2)
-            {
-                Console.WriteLine("Pressure: {0} barg", Result[i].ToString());
-                Console.WriteLine("Temperature: {0} °C", Result[i + 1].ToString());
-                Console.WriteLine();
-            }
-
-            double[] Dens_Result = PhaseOpt.PhaseOpt.Calculate_Density_And_Compressibility(IDs, Values);
-
-            Console.WriteLine("Vapour density: {0} kg/m­³", Dens_Result[0]);
-            Console.WriteLine("Compressibility factor: {0}", Dens_Result[2]);
-            Console.WriteLine("Liquid density: {0} kg/m­³", Dens_Result[1]);
-            Console.WriteLine("Compressibility factor: {0}", Dens_Result[3]);
-
-            return;
-        }
 
         System.IO.StreamWriter Log_File = System.IO.File.AppendText(@"PhaseOpt_Kar.log");
 
@@ -488,7 +444,7 @@ public static class Tester
         Log_File.Close();
     }
 
-    public static void Read_Config(string Config_File)
+    public void Read_Config(string Config_File)
     {
         XmlReaderSettings settings = new XmlReaderSettings();
         settings.IgnoreComments = true;
@@ -713,7 +669,7 @@ public static class Tester
         }
     }
 
-    public static Hashtable Read_Values(string[] Tag_Name, DateTime Time_Stamp)
+    public Hashtable Read_Values(string[] Tag_Name, DateTime Time_Stamp)
     {
         string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
 
@@ -749,7 +705,7 @@ WHERE
         return Result;
     }
 
-    public static void Write_Value(string Tag_Name, double Value, string Quality = "Good")
+    public void Write_Value(string Tag_Name, double Value, string Quality = "Good")
     {
         string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
 
@@ -766,7 +722,7 @@ WHERE
         Cmd.Connection.Close();
     }
 
-    private static bool Check_Composition(List<Component> Composition)
+    private bool Check_Composition(List<Component> Composition)
     {
         bool Return_Value = true;
         double Expected_Sum = 100.0;
