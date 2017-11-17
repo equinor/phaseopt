7cd8c9aa4e4593d68bb16021cc2a1aeb914d8bac
Roy Vegard Ovesen <rovo@statoil.com>
2017-11-17 16:35:02 +0100

    Refactor IP21 communication and add testing class

---
+++ IP21_Comm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IP21_Comm
+{
+    public string IP21_Host;
+    public string IP21_Port;
+    private System.Data.Odbc.OdbcCommand Cmd;
+
+    public IP21_Comm(string Host, string Port)
+    {
+        IP21_Host = Host;
+        IP21_Port = Port;
+    }
+
+    public void Connect()
+    {
+        string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
+        Cmd = new System.Data.Odbc.OdbcCommand();
+        Cmd.Connection = new System.Data.Odbc.OdbcConnection(Conn);
+        Cmd.Connection.Open();
+    }
+
+    public void Disconnect()
+    {
+        if (Cmd.Connection.State == System.Data.ConnectionState.Open)
+        {
+            Cmd.Connection.Close();
+        }
+    }
+
+    public Hashtable Read_Values(string[] Tag_Name, DateTime Time_Stamp)
+    {
+        string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
+
+        System.Data.Odbc.OdbcCommand Cmd = new System.Data.Odbc.OdbcCommand();
+        Cmd.Connection = new System.Data.Odbc.OdbcConnection(Conn);
+        Cmd.Connection.Open();
+
+        string Tag_cond = "(FALSE"; // Makes it easier to add OR conditions.
+        foreach (string Tag in Tag_Name)
+        {
+            Tag_cond += "\n  OR NAME = '" + Tag + "'";
+        }
+        Tag_cond += ")";
+        Cmd.CommandText =
+@"SELECT
+  NAME, VALUE
+FROM
+  History
+WHERE
+  " + Tag_cond + @"
+  AND TS > CAST('" + Time_Stamp.AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss") + @"' AS TIMESTAMP FORMAT 'YYYY-MM-DD HH:MI:SS')
+  AND TS < CAST('" + Time_Stamp.AddSeconds(1).ToString("yyyy-MM-dd HH:mm:ss") + @"' AS TIMESTAMP FORMAT 'YYYY-MM-DD HH:MI:SS')
+  AND PERIOD = 000:0:01.0;
+";
+
+        System.Data.Odbc.OdbcDataReader DR = Cmd.ExecuteReader();
+        Hashtable Result = new Hashtable();
+        while (DR.Read())
+        {
+            Result.Add(DR.GetValue(0), DR.GetValue(1));
+        }
+        Cmd.Connection.Close();
+        return Result;
+    }
+
+    public void Write_Value(string Tag_Name, double Value, string Quality = "Good")
+    {
+        string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
+
+        System.Data.Odbc.OdbcCommand Cmd = new System.Data.Odbc.OdbcCommand();
+        Cmd.Connection = new System.Data.Odbc.OdbcConnection(Conn);
+        Cmd.Connection.Open();
+
+        Cmd.CommandText =
+@"UPDATE ip_analogdef
+  SET ip_input_value = " + Value.ToString() + @", ip_input_quality = '" + Quality + @"'
+  WHERE name = '" + Tag_Name + @"'";
+
+        System.Data.Odbc.OdbcDataReader DR = Cmd.ExecuteReader();
+        Cmd.Connection.Close();
+    }
+
+    public void Write_Value(string Tag_Name, int Value, string Quality = "Good")
+    {
+        string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
+
+        System.Data.Odbc.OdbcCommand Cmd = new System.Data.Odbc.OdbcCommand();
+        Cmd.Connection = new System.Data.Odbc.OdbcConnection(Conn);
+        Cmd.Connection.Open();
+
+        Cmd.CommandText =
+@"UPDATE ip_discretedef
+  SET ip_input_value = " + Value.ToString() + @", ip_input_quality = '" + Quality + @"'
+  WHERE name = '" + Tag_Name + @"'";
+
+        System.Data.Odbc.OdbcDataReader DR = Cmd.ExecuteReader();
+        Cmd.Connection.Close();
+    }
+
+    public void Write_Value(string Tag_Name, string Value, string Quality = "Good")
+    {
+        string Conn = @"DRIVER={AspenTech SQLplus};HOST=" + IP21_Host + @";PORT=" + IP21_Port;
+
+        System.Data.Odbc.OdbcCommand Cmd = new System.Data.Odbc.OdbcCommand();
+        Cmd.Connection = new System.Data.Odbc.OdbcConnection(Conn);
+        Cmd.Connection.Open();
+
+        Cmd.CommandText =
+@"UPDATE ip_textdef
+  SET ip_input_value = '" + Value + @"', ip_input_quality = '" + Quality + @"'
+  WHERE name = '" + Tag_Name + @"'";
+
+        System.Data.Odbc.OdbcDataReader DR = Cmd.ExecuteReader();
+        Cmd.Connection.Close();
+    }
+
+    public void Insert_Value(string Tag_Name, double Value, DateTime Time_Stamp)
+    {
+        Cmd.CommandText =
+@"INSERT INTO " + Tag_Name + @"(IP_TREND_TIME, IP_TREND_VALUE)
+  VALUES (CAST('" + Time_Stamp.ToString("yyyy-MM-dd HH:mm:ss") + @"' AS TIMESTAMP FORMAT 'YYYY-MM-DD HH:MI:SS'), "
+                  + Value.ToString() + @");";
+
+        Cmd.ExecuteNonQuery();
+    }
+}
---
+++ Testing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_Space
+{
+    public static class Testing
+    {
+        public static void Main()
+        {
+            IP21_Comm DB_Connection = new IP21_Comm("KAR-IP21.statoil.net", "10014");
+
+            DB_Connection.Connect();
+            DB_Connection.Insert_Value("PO_E_P0", 100.354, DateTime.Now.AddSeconds(-20));
+
+            DB_Connection.Disconnect();
+
+        }
+    }
+}
