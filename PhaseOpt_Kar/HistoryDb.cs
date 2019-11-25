68776b210dfe39f9ad129e85e399b1b7c87cced8
Roy Vegard Ovesen <rovo@equinor.com>
2019-11-25 19:22:39 +0100

    Add SQLite packages

---
+++ PhaseOpt_Kar/HistoryDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhaseOpt
+{
+    public class HistoryDb
+    {
+        public static double GetValue(string Tag, DateTime TimeStamp)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                return 0.0;
+            }
+        }
+
+        public static void PutValue(string Tag, DateTime TimeStamp, double Value)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                //cnn.Execute("insert into History (TimeStamp, Tag, Value) values (@TimeStamp, @Tag, @Value)");
+            }
+
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
--- PhaseOpt_Kar/Main.cs
+++ PhaseOpt_Kar/Main.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Threading.Tasks;
 
-namespace Main
+namespace PhaseOpt
 {
     public static class Main_Class
     {
